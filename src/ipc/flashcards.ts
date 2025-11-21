a45785fc5e311e2dc0499ccbe887a7c95fc47054
pineapp1es <supdevpine@gmail.com>
2025-11-21 23:58:56 +0530

    added ipc handlers for flashcard functions

--- src/ipc/flashcards.ts
+++ src/ipc/flashcards.ts
@@ -1,12 +1,13 @@
+import { ipcMain } from 'electron';
 import { messageType, questionType, questionAttemptType, updateQuestionsInDeck } from './database/decks.ts';
 import { decks } from './loadDecks.ts';
 
 export const addMessageToChatInFlashcard = async (deckId: String, message: messageType, flashcardId: String) => {
     for (let deck of decks) {
         if (deck['id'] == deckId) {
-            let questions: [ questionType ] = deck['questions'];
+            let questions: [questionType] = deck['questions'];
             for (let question of questions) {
-                if (question['id'] ==  flashcardId) {
+                if (question['id'] == flashcardId) {
                     question['chat'].push(message);
                     updateQuestionsInDeck(deckId, questions)
                 }
@@ -18,9 +19,9 @@ export const addMessageToChatInFlashcard = async (deckId: String, message: messa
 export const addNewAttemptToFlashcard = async (deckId: String, attemptData: questionAttemptType, flashcardId: String) => {
     for (let deck of decks) {
         if (deck['id'] == deckId) {
-            let questions: [ questionType ] = deck['questions'];
+            let questions: [questionType] = deck['questions'];
             for (let question of questions) {
-                if (question['id'] ==  flashcardId) {
+                if (question['id'] == flashcardId) {
                     question['attempts'].push(attemptData);
                     updateQuestionsInDeck(deckId, questions)
                 }
@@ -31,6 +32,8 @@ export const addNewAttemptToFlashcard = async (deckId: String, attemptData: ques
 
 export const getFlashcardReviewQueue = async (deckId: String) => {
     let questions: Array<questionType> = [];
+    let newQuestions: number = 0;
+    let learning: number = 0;
     for (let deck of decks) {
         if (deck['id'] == deckId) {
             questions = deck['questions'];
@@ -47,9 +50,23 @@ export const getFlashcardReviewQueue = async (deckId: String) => {
             reviewQueue.push({ question: question, due: due });
     }
 
-    reviewQueue.sort( (a, b) => {
+    reviewQueue.sort((a, b) => {
         return a.due - b.due;
     });
 
-    return reviewQueue;
+    return [ reviewQueue, newQuestions, learning ] as const;
+}
+
+
+export function registerIpcFunctions() {
+    ipcMain.handle('flashcard:addMessageToChatInFlashcard ',
+        (event, deckId: String, message: messageType, flashcardId: String) =>
+            addMessageToChatInFlashcard(deckId, message, flashcardId));
+
+    ipcMain.handle('flashcard:addNewAttemptToFlashcard',
+        (event, deckId: String, attemptData: questionAttemptType, flashcardId: String) =>
+            addNewAttemptToFlashcard(deckId, attemptData, flashcardId));
+
+    ipcMain.handle('flashcard:getFlashCardReview',
+        (event, deckId: String) => getFlashcardReviewQueue(deckId));
 }
