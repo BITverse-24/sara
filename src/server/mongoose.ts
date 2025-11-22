70b3f1776adcdc44d3152c867a183f2d43c9d5aa
pineapp1es <supdevpine@gmail.com>
2025-11-22 10:24:37 +0530

    fixes 3

--- src/server/mongoose.ts
+++ src/server/mongoose.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://pineapple:pineapple@cluster0.ijignp2.mongodb.net/?appName=Cluster0', { dbName: "sara" });
+mongoose.connect('', { dbName: "sara" });
 
 export default mongoose;
